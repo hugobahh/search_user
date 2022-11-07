62ff1ddba5c78ef2570761b3316649368ee2dfa8
Hugo BH <42075446+hugobahh@users.noreply.github.com>
2022-11-07 10:41:45 -0600

    Base
    Version_1

---
+++ utils/utils.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+//=====================  Archivo Conf ==========================================
+func GetIpPuerto() string {
+	sIpPort := ""
+	fmt.Println(sIpPort)
+	viper.SetConfigFile("configHttp.json")
+	if err := viper.ReadInConfig(); err != nil {
+		//panic(fmt.Errorf("Fatal error in config json file: %s ", err))
+		return sIpPort
+	}
+
+	sPort := fmt.Sprintf(":%s", viper.GetString("HTTP.port"))
+	sPort = strings.Replace(fmt.Sprint(sPort), ":", "", 1)
+	sIp := fmt.Sprintf(":%s", viper.GetString("HTTP.ip"))
+	sIp = strings.Replace(fmt.Sprint(sIp), ":", "", 1)
+	if sPort == "0" || sPort == "" {
+		sIpPort = fmt.Sprint(sIp)
+	} else {
+		sIpPort = fmt.Sprint(sIp) + ":" + fmt.Sprint(sPort)
+	}
+	return sIpPort
+} //FIN de ObtenerIpPuerto
+
+func ObtenerPuerto() string {
+	sPort := ""
+	fmt.Println(sPort)
+	viper.SetConfigFile("configHttp.json")
+	if err := viper.ReadInConfig(); err != nil {
+		//panic(fmt.Errorf("Fatal error in config json file: %s ", err))
+		return sPort
+	}
+
+	sPort = fmt.Sprintf(":%s", viper.GetString("HTTP.port"))
+	return sPort
+} //FIN de ObtenerPort
